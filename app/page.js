f4639aa809930a26e30b779ac655811542dd88b4
Juli Habif <julieta-bv@hotmail.com>
2023-10-15 19:20:16 -0300

    structure

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import styles from './page.module.css'
-import {MoviesWrapper} from "@/app/components/MoviesWrapper";
+import {MoviesWrapper} from "./components/MoviesWrapper";
 
 export default function Home() {
   return (
